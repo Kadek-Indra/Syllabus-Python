06611f03dcfe3b3a078278fa910a73629b90bfd7
Kadek-Indra <IKIndra.JPutra@mitrais.com>
2026-07-31 14:04:32 +0700

    Topic3 completed

---
+++ Declare the variables.py
@@ -0,0 +1,19 @@
+nama = "Indra"
+umur = 16
+tinggi = 170.5
+mahasiswa = False
+siswa = True
+
+print (f"nama saya              :  {nama}")
+print (f"umur saya              :  {umur}")
+print (f"tinggi saya            :  {tinggi}")
+print (f"apakah saya mahasiswa? :  {mahasiswa}")
+print (f"apakah saya siswa?     :  {siswa}")
+
+print ("")
+
+print (f"nama      = {type(nama)}")
+print (f"umur      = {type(umur)}")
+print (f"tinggi    = {type(tinggi)}")
+print (f"mahasiswa = {type(mahasiswa)}")
+print (f"siswa     = {type(siswa)}")
