56ada204781041bc4942d523f3f95ee8a9da7e40
Kadek-Indra <IKIndra.JPutra@mitrais.com>
2026-08-03 12:13:45 +0700

    Topic9 completed

---
+++ countdown timer.py
@@ -0,0 +1,20 @@
+# sss
+import time
+
+second = int(input("Enter the time in seconds : "))
+
+while second > 0:
+    seconds = second % 60
+    minutes = (second // 60) % 60
+    hours = (second // 3600)
+    print (f"{hours:02}:{minutes:02}:{seconds:02}")
+    second -=1
+    time.sleep(1)
+    if second <= 10:
+        break
+
+for x in range(second, 0, -1):
+    print(f"00:00:{x:02} seconds left!")
+    time.sleep(1)
+
+print ("Time's Up!")
