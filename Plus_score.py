d8264a56c54c7874adc6acb46a271210c3c90cba
Kadek-Indra <IKIndra.JPutra@mitrais.com>
2026-08-06 15:51:55 +0700

    Topic2.2 was completed

--- Plus_score.py
+++ Plus_score.py
@@ -1,7 +1,7 @@
 # list comprehensions
 
 original_scores = [75, 82, 90 ,68, 95]
-updated_scores = [num + 5 for num in original_scores ]
+updated_score = [num + 5 for num in original_scores ]
 
 print (f"Your score in the first     : {original_scores}")
-print (f"Your score after plus score : {updated_scores}")
+print (f"Your score after plus score : {updated_score}")
